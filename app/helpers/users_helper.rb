5230d54429ca0231501117c1861758e086c7c54d
rohanphillips <rohan@solutions-insurance.com>
2020-09-23 10:39:36 -0500

    working on users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def submit_text
+    if current_page?(:controller => 'users', :action => 'edit', :id => @user.id.to_s) 
+      "Save"
+    elsif current_page?(:controller => 'users', :action => 'new') 
+      "Create"
+    end
+  end
 end
