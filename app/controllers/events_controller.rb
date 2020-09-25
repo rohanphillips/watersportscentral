c1929ea0a9142b5a001fbd8262148f8afb8632d2
rohanphillips <rohan@solutions-insurance.com>
2020-09-25 06:42:09 -0500

    Adding Events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,51 @@
+class EventsController < ApplicationController
+  skip_before_action :authorized, only: [:index]
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.valid?
+      @event = Event.create(event_params)
+      render :show
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.valid?
+      if params[:commit] != "Edit"
+        Event.update(event_params)
+        redirect_to event_url
+      else
+        redirect_to edit_event_url
+      end
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @events = Event.all
+  end
+ 
+  private
+ 
+  def event_params
+    params.require(:event).permit(:name, :description, :sport, :date, :location).merge(user_id: session[:user_id])
+  end
+
+
+end
